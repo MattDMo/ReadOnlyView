0be14be8c3c8d5e36ebc6e12a363f5a9c57be8db
MattDMo <mattdmo@pigimal.com>
2016-05-31 22:24:49 -0400

    Formatting, docstrings, get settings right, tweaks

--- read_only_view.py
+++ read_only_view.py
@@ -6,20 +6,29 @@ from os.path import split as opsplit
 
 class MakeViewReadOnlyCommand(sublime_plugin.TextCommand):
     def run(self, edit):
+        """Set view to read-only"""
+
         self.view.set_read_only(True)
 
 
 class MakeViewWriteableCommand(sublime_plugin.TextCommand):
     def run(self, edit):
+        """Set view to writeable"""
+
         self.view.set_read_only(False)
 
 
 class ToggleReadOnlyViewCommand(sublime_plugin.ApplicationCommand):
-    def is_checked(self, view):
-        """Returns whether "Read-Only View" menu items should be checked"""
+    def is_checked(self):
+        """Returns whether "Read-Only View" menu items should be checked."""
+
+        view = sublime.active_window().active_view()
         return view.is_read_only()
 
-    def run(self, view):
+    def run(self):
+        """Check if view is read-only, if so then make writeable. If not, make read-only."""
+
+        view = sublime.active_window().active_view()
         if view.is_read_only():
             view.run_command("make_view_writeable")
         else:
@@ -28,14 +37,20 @@ class ToggleReadOnlyViewCommand(sublime_plugin.ApplicationCommand):
 
 class ReadOnlyFilesListener(sublime_plugin.EventListener):
     def on_load_async(self, view):
+        """Every time a new view is loaded, check if its extension or filename is in settings.
+           If so, set as read-only.
+        """
+
         filename = view.file_name()
+        # view hasn't been saved yet: don't worry about it
         if not filename:
             return
 
-        s = view.settings("Read-Only View.sublime-settings")
+        s = sublime.load_settings("Read-Only View.sublime-settings")
         extensions = s.get("read_only_extensions", [])
         filenames = s.get("read_only_filenames", [])
-        if not extensions or not filenames:
+        # if both lists are empty, don't do anything
+        if not extensions and not filenames:
             return
 
         if extensions:
@@ -47,3 +62,5 @@ class ReadOnlyFilesListener(sublime_plugin.EventListener):
             for file in filenames:
                 if file == opsplit(filename)[1]:
                     view.run_command("make_view_read_only")
+
+        # TODO: Figure out some way of matching regex file patterns
